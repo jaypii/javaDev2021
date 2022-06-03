92557af19bd828daaf360a184055dc05921ace69
jaypii <jpomv2@icloud.com>
2022-06-03 09:05:45 +0200

    Update java Dev directory

---
+++ my-app/src/main/java/com/jaypii/app/App.java
@@ -0,0 +1,13 @@
+package com.jaypii.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
